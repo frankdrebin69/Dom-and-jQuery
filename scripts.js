322b93400bd5335177c54fe73b96d37f4f2a4fe9
frankdrebin69 <gaborvamos95@gmail.com>
2021-08-04 03:56:04 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,10 +4,10 @@ arraywithnames.forEach(namesLoop)
 
 function namesLoop(dataarray) {
     if (dataarray === "Péter") {
-        $("ul").append("<li class='myname'>" + dataarray + "</li>")
-        $(".myname").css("font-weight", "bold")
+        $('ul').append("<li class='myname'>" + dataarray + "</li>")
+        $('.myname').css("font-weight", "bold")
     } else {
-        $("ul").append("<li>" + dataarray + "</li>")
+        $('ul').append("<li>" + dataarray + "</li>")
     }
 }
 
@@ -16,15 +16,15 @@ var additionalblock = {
     text: "Ez a blokk a JavaScript jQuery könyvtárával lett beillesztve. Király!"
 }
 
-$("body").append("<h1>" + additionalblock.title + "</h1>")
+$('body').append("<h1>" + additionalblock.title + "</h1>")
 
-$("body").append("<p>" + additionalblock.text + "</p>")
+$('body').append("<p>" + additionalblock.text + "</p>")
 
 var additionalcontent = {
     title: "Listák és szövegek",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus pellentesque eu tincidunt tortor."
 }
 
-$("body").append("<p>" + additionalcontent.text  + "</p>")
+$('body').append("<p>" + additionalcontent.text  + "</p>")
 
-$("body").append("<h1>" + additionalcontent.title + "</h1>")
+$('body').append("<h1>" + additionalcontent.title + "</h1>")
